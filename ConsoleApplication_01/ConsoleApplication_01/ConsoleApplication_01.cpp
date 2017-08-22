c192c21b02b86a67a0e8b1debd7074c33820063a
junshanlee <junshanlee@gmail.com>
2017-08-22 14:54:44 +0800

    new project_ok

---
+++ ConsoleApplication_01/ConsoleApplication_01/ConsoleApplication_01.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication_01.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include <cstdlib>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	printf("test01\n");
+	system("pause");
+	return 0;
+}
+
